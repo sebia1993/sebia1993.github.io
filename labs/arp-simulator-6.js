eb27eb088ec09828243ae458b1b7799d9a0d1510
sebia1993 | 네트워크 자동화 엔지니어 <49932643+sebia1993@users.noreply.github.com>
2026-09-21 16:26:30 +0900

    Align ARP mobile cache after prediction click fix

--- labs/arp-simulator-6.js
+++ labs/arp-simulator-6.js
@@ -158,7 +158,7 @@ setViewMode("basic");
 configureLesson(0);
 
 (function loadMobileSimulatorLayer(){
-  const version="20260921-mode15";
+  const version="20260921-mode16";
   const css=document.createElement("link");
   css.rel="stylesheet";
   css.href=`arp-simulator-mobile.css?v=${version}`;
