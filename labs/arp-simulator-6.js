6f6020b53fda325320da635beb00252aa4a019d6
sebia1993 | 네트워크 자동화 엔지니어 <49932643+sebia1993@users.noreply.github.com>
2026-09-21 15:15:00 +0900

    Align ARP mobile assets with prediction-first reveal

--- labs/arp-simulator-6.js
+++ labs/arp-simulator-6.js
@@ -158,7 +158,7 @@ setViewMode("basic");
 configureLesson(0);
 
 (function loadMobileSimulatorLayer(){
-  const version="20260921-mode14";
+  const version="20260921-mode15";
   const css=document.createElement("link");
   css.rel="stylesheet";
   css.href=`arp-simulator-mobile.css?v=${version}`;
